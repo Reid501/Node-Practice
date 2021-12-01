20e9d634791b1acdae450353b99fd55093de728b
Alex <alexatrs@hotmail.co.uk>
2021-12-01 19:12:32 +0000

    ejs files added

--- server/app.js
+++ server/app.js
@@ -2,26 +2,28 @@ const express = require('express');
 
 const app = express();
 
+// Register view engine
+app.set('view engine', 'ejs');
+
 // Listen for requests
 
 app.listen(3000);
 
 app.get('/', (req, res) => {
     // res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname});
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
     // res.send('<p>about page</p>');
-    res.sendFile('./views/about.html', { root: __dirname});
+    res.render('about');
 });
-
-// Redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+ 
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
 });
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname});
+    res.status(404).render('404');
 });
